aca84c4e54b17214e153578e325266822da9e754
Vojtěch Perník <develop@pervoj.cz>
2022-06-15 15:23:29 +0200

    Enhance translation file

--- src/i18n.js
+++ src/i18n.js
@@ -10,25 +10,41 @@ i18next
 		resources: {
 			en: {
 				translation: {
+					// currencies
 					"currency-code": "EUR",
 					"currency-str": "€ {price}",
 					"currency-format": (v) => v.toFixed(2),
+
+					// products
 					"products-not-found": "No products found",
 					"product-prev-price": "previous price",
 					"product-add-to-cart": "Add to Cart",
 					"product-more-info": "More Info",
+
+					// footer
+					"footer-github": "Source code on GitHub",
+
+					// 404
 					"not-found": "This page was not found! 🙁️",
 				},
 			},
 			cs: {
 				translation: {
+					// currencies
 					"currency-code": "CZK",
 					"currency-str": "{price} Kč",
-					"currency-format": (v) => Math.round(v),
+					"currency-format": (v) => Math.ceil(v),
+
+					// products
 					"products-not-found": "Žádné produkty nebyly nalezeny",
 					"product-prev-price": "předchozí cena",
 					"product-add-to-cart": "Přidat do košíku",
 					"product-more-info": "Více informací",
+
+					// footer
+					"footer-github": "Zdrojový kód na GitHubu",
+
+					// 404
 					"not-found": "Tato stránka nebyla nalezena! 🙁️",
 				},
 			},
