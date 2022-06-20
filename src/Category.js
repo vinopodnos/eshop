b4581c5e0c1600c9a2d38d76d78bd2e4685314e0
Vojtěch Perník <develop@pervoj.cz>
2022-06-20 14:30:36 +0200

    Read category ID

--- src/Category.js
+++ src/Category.js
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/products/ProductList";
@@ -6,11 +7,12 @@ import styles from "./Category.module.scss";
 import data from "./temporaryData";
 
 export default function Category() {
+	const { categoryId } = useParams();
 	return (
 		<>
 			<Navbar />
 			<main className={styles["content"]}>
-				<h1>Červené víno</h1>
+				<h1>Category {categoryId}</h1>
 				<ProductList products={data} />
 			</main>
 			<Footer />
