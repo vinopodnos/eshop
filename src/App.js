b7f7e7d031e719d1ef46b6bd2f595c76ac1b0e1a
Vojtěch Perník <develop@pervoj.cz>
2022-06-21 14:07:26 +0200

    Fix auto scrolling to top

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 import Home from "./Home";
 import Categories from "./Categories";
 import Category from "./Category";
@@ -8,13 +9,18 @@ import NotFound from "./NotFound";
 export default function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/categories" element={<Categories />} />
-				<Route path="/categories/:categoryId" element={<Category />} />
-				<Route path="/products/:productId" element={<Product />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
+			<ScrollToTop>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/categories" element={<Categories />} />
+					<Route
+						path="/categories/:categoryId"
+						element={<Category />}
+					/>
+					<Route path="/products/:productId" element={<Product />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</ScrollToTop>
 		</BrowserRouter>
 	);
 }
