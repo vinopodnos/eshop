8fcce2f23c993e5de5f2424b0476a856922eb8d7
Vojtěch Perník <develop@pervoj.cz>
2022-06-23 16:04:01 +0200

    Remove aditional import

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 const CartContext = createContext({});
